208e73205f6a6a2e999ad840c073ad366a47a19c
nssina <rabie_sina@yahoo.com>
2020-11-19 14:19:24 -0500

    New share

--- calScript.swift
+++ calScript.swift
@@ -6,7 +6,6 @@ func calForNima(total: Double, nimaShare: Double) -> Double {
     return nimaShare * 100 / total
 }
 
-print(calForSina(total: 90, sinaShare: 20))
+print(calForSina(total: 140, sinaShare: 20))
 
-
-print(calForNima(total: 90, nimaShare: 70))
+print(calForNima(total: 140, nimaShare: 120))
