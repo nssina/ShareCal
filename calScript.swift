f786d8230fb2751442d5bcec10bd210bf2f090fc
nsdevelopteam <63967267+nsdevelopteam@users.noreply.github.com>
2020-11-19 10:46:26 -0500

    Initial commite

---
+++ calScript.swift
@@ -0,0 +1,12 @@
+func calForSina(total: Double, sinaShare: Double) -> Double {
+    return sinaShare * 100 / total
+}
+
+func calForNima(total: Double, nimaShare: Double) -> Double {
+    return nimaShare * 100 / total
+}
+
+print(calForSina(total: 90, sinaShare: 20))
+
+
+print(calForNima(total: 90, nimaShare: 70))
